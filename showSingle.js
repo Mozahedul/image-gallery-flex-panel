f1334c19e1869e82bb59a1a18e0627133505ec1d
Mozahedul <mozahed001@gmail.com>
2022-05-13 22:59:43 +0600

    Show single panel at a time & minimize others
    This will show only one panel at a time and minimize others.

---
+++ showSingle.js
@@ -0,0 +1,27 @@
+const panels = document.querySelectorAll(".panel");
+
+function toggleOpen(e) {
+  let item = document.querySelector(".open");
+
+  // remove all classes with .open
+  if (item !== null) {
+    item.classList.remove("open");
+  }
+
+  // add the class .open to current target element
+  if (!e.currentTarget.classList.contains("open")) {
+    this.classList.add("open");
+  }
+  return;
+}
+
+function toggleActive(e) {
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
+
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleActive),
+);
